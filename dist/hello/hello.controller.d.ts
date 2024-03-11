c42ffc2bfdf19fd7defa7b3f0693644df9b2f6b3
Jinseung <127307160+wlstmd@users.noreply.github.com>
2024-03-11 20:14:32 +0900

    Delete dist directory

--- dist/hello/hello.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { HelloService } from './hello.service';
-export declare class HelloController {
-    private readonly helloSerivce;
-    constructor(helloSerivce: HelloService);
-    getHello(): string;
-}
